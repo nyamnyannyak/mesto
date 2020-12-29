6e5275eaa980faafac5c5136e6708ecc202c8ada
Liza Vakhrusheva <nyamnyannyak@yandex.ru>
2020-12-30 00:27:35 +0300

    fix: обновлять значения value при открытии попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,32 @@
-let editButton = document.querySelector('.profile__edit-button')
-let popup = document.querySelector('.popup')
-let closeButton = popup.querySelector('.popup__close-button')
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__field_content_name');
+let jobInput = formElement.querySelector('.popup__field_content_job');
+
 
 function togglePopup () {
-  popup.classList.toggle ('popup_opened')
+  popup.classList.toggle ('popup_opened');
+}
+function openPopup () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  togglePopup ();
 }
 
-editButton.addEventListener('click', togglePopup); 
+editButton.addEventListener('click', openPopup); 
 closeButton.addEventListener('click', togglePopup); 
 
-let formElement = popup.querySelector('.popup__form')
 
-let profileName = document.querySelector('.profile__name')
-let profileJob = document.querySelector('.profile__description') 
-let nameInput = formElement.querySelector('.popup__field_content_name')
-let jobInput = formElement.querySelector('.popup__field_content_job')
 
-nameInput.value = profileName.textContent
-jobInput.value = profileJob.textContent
+
+
+
+
+
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
