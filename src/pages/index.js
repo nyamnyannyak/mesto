7c02acc2d394fd45fca7f1cca7e5eb66a56b24c5
Liza Vakhrusheva <nyamnyannyak@yandex.ru>
2021-03-12 18:29:53 +0300

    reactor: файловая структура

---
+++ src/pages/index.js
@@ -0,0 +1,89 @@
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import { initialCards } from '../utils/initial-cards.js';
+import { 
+  profileFormElement,
+  addFormElement,
+  elements,
+  addButton,
+  editButton,
+  nameInput,
+  jobInput,
+  formSelectors
+} from '../utils/constants.js';
+
+//создание секции с карточками
+const cardsList = new Section({
+  items: initialCards,
+  renderer: renderer 
+  },
+  elements
+)
+// отрисовка карточек из массива
+cardsList.renderItems();
+
+//функция создания и отрисовки карточек
+function renderer (item) {
+  const card = new Card(item, {
+    cardSelector: '#element-template',
+    handleCardClick: (name, link) => {
+      const photoPopup = new PopupWithImage('.photo-popup');
+      photoPopup.open(name, link);
+      photoPopup.setEventListeners();
+    }
+  });
+  const cardElement = card.generateCard();
+  cardsList.addItem(cardElement);
+  }
+
+
+//валидация
+const addFormValidator = new FormValidator(formSelectors, addFormElement);
+const editFormValidator = new FormValidator(formSelectors, profileFormElement);
+addFormValidator.enableValidation()
+editFormValidator.enableValidation();
+
+//попапы
+const info = new UserInfo ('.profile__name', '.profile__description');
+const popupWithEditForm = new PopupWithForm ({ 
+  popupSelector: '.popup_type_edit', 
+  handleFormSubmit: (data) => {
+    info.setUserInfo(data);
+    popupWithEditForm.close();
+  }
+})
+popupWithEditForm.setEventListeners();
+
+const popupWithAddForm = new PopupWithForm ({ 
+  popupSelector: '.popup_type_add', 
+  handleFormSubmit: (item) => {
+      renderer(item);
+      popupWithAddForm.close();
+  }
+})
+popupWithAddForm.setEventListeners();
+
+
+function handleEditButtonClick () {
+  const userInfo = info.getUserInfo();
+  nameInput.value = userInfo.name;
+  jobInput.value = userInfo.job;
+  popupWithEditForm.open();
+  editFormValidator.resetFormState();
+}
+
+function handleAddButtonClick () {
+  popupWithAddForm.open();
+  addFormValidator.resetFormState();
+}
+
+editButton.addEventListener('click', handleEditButtonClick); 
+addButton.addEventListener('click',  handleAddButtonClick); 
+
+
+
+
---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const profileFormElement = document.querySelector('.popup_type_edit').querySelector('.popup__form');
+export const addFormElement = document.querySelector('.popup_type_add').querySelector('.popup__form');
+export const elements = '.elements';
+export const addButton = document.querySelector('.profile__add-button');
+export const editButton = document.querySelector('.profile__edit-button');
+export const nameInput = profileFormElement.querySelector('.popup__input_content_name');
+export const jobInput = profileFormElement.querySelector('.popup__input_content_job');
+
+//настройки для валидации
+export const formSelectors = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_visible',
+  };
